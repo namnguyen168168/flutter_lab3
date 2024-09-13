719287f23fe3e2cd9d8b7377c1904aba86b63d91
Nguyen Hoang Nam <109569265+namnguyen168168@users.noreply.github.com>
2024-09-13 22:18:23 +0700

    Add files via upload

---
+++ main.dart
@@ -0,0 +1,109 @@
+import 'dart:convert';
+import 'dart:io';
+import 'package:flutter/material.dart';
+import 'package:path_provider/path_provider.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Todo List',
+      home: TodoListPage(),
+    );
+  }
+}
+
+class TodoListPage extends StatefulWidget {
+  @override
+  _TodoListPageState createState() => _TodoListPageState();
+}
+
+class _TodoListPageState extends State<TodoListPage> {
+  List<String> _todos = [];
+  final TextEditingController _controller = TextEditingController();
+
+  @override
+  void initState() {
+    super.initState();
+    _loadTodos();
+  }
+
+  Future<void> _loadTodos() async {
+    final file = await _getLocalFile();
+    if (file.existsSync()) {
+      String contents = await file.readAsString();
+      setState(() {
+        _todos = List<String>.from(json.decode(contents));
+      });
+    }
+  }
+
+  Future<File> _getLocalFile() async {
+    final directory = await getApplicationDocumentsDirectory();
+    return File('${directory.path}/todos.json');
+  }
+
+  Future<void> _saveTodos() async {
+    final file = await _getLocalFile();
+    await file.writeAsString(json.encode(_todos));
+  }
+
+  void _addTodo() {
+    if (_controller.text.isNotEmpty) {
+      setState(() {
+        _todos.add(_controller.text);
+        _controller.clear();
+      });
+      _saveTodos();
+    }
+  }
+
+  void _removeTodoAt(int index) {
+    setState(() {
+      _todos.removeAt(index);
+    });
+    _saveTodos();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Todo List')),
+      body: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: TextField(
+              controller: _controller,
+              decoration: InputDecoration(
+                labelText: 'Nhập công việc',
+                suffixIcon: IconButton(
+                  icon: Icon(Icons.add),
+                  onPressed: _addTodo,
+                ),
+              ),
+            ),
+          ),
+          Expanded(
+            child: ListView.builder(
+              itemCount: _todos.length,
+              itemBuilder: (context, index) {
+                return ListTile(
+                  title: Text(_todos[index]),
+                  trailing: IconButton(
+                    icon: Icon(Icons.delete),
+                    onPressed: () => _removeTodoAt(index),
+                  ),
+                );
+              },
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
